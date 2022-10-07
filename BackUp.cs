e286345cdbabfe04ebbc9f7265e9bb58c603b572
silence2k6 <robert.leithner@gmail.com>
2022-10-07 03:03:04 +0200

    working on XmlSerializer

--- BackUp.cs
+++ BackUp.cs
@@ -4,24 +4,15 @@ namespace QuizMaker
 {
     public class BackUp
     {            
-        public static void CreateRepositoryBackup(List<QuizCard> quizCardRepository)
+        public static void CreateRepositoryBackup(List<QuizCard> quizCardRepository, XmlSerializer writer, string path)
         {
-            XmlSerializer writer = new XmlSerializer(typeof(List<QuizCard>));
-
-            string path = @"C:\Users\user\source\repos\QuizMaker\QuizCardRepository.xml";
-
             using (FileStream file = File.Create(path))
             {
                 writer.Serialize(file, quizCardRepository);
             }
-
         }
-        public static List<QuizCard> GetRepositoryBackup(List<QuizCard> quizCardRepository)
+        public static List<QuizCard> GetRepositoryBackup(List<QuizCard> quizCardRepository, XmlSerializer reader, string path)
         {
-            XmlSerializer reader = new XmlSerializer(typeof(List<QuizCard>));
-
-            string path = @"C:\Users\user\source\repos\QuizMaker\QuizCardRepository.xml";
-
             using (FileStream file = File.OpenRead(path))
             {
                 quizCardRepository = reader.Deserialize(file) as List<QuizCard>;
--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿namespace QuizMaker
+﻿using System.Xml.Serialization;
+
+namespace QuizMaker
 {
     internal class Program
-    {
+    {       
         static void Main(string[] args)
         {
+            XmlSerializer writer = new XmlSerializer(typeof(List<QuizCard>));
+            XmlSerializer reader = new XmlSerializer(typeof(List<QuizCard>));
+            var path = @"C:\Users\user\source\repos\QuizMaker\QuizCardRepository.xml";
+
             List<QuizCard> quizcardRepository = new List<QuizCard>();
-            
+
             UserInterface.GameIntroMessage();
 
             int createOrPlay = UserInterface.CreateOrPlayMessage();
@@ -16,11 +22,11 @@
 
                 while (createQuizcards == true)
                 {
-                    quizcardRepository = BackUp.GetRepositoryBackup(quizcardRepository);
+                    quizcardRepository = BackUp.GetRepositoryBackup(quizcardRepository, reader, path);
                     DataInterface.CreateQuestion(quizcardRepository);
-                    BackUp.CreateRepositoryBackup(quizcardRepository);
                     createQuizcards = UserInterface.CreateOneMoreQuizcard();
                 }
+                BackUp.CreateRepositoryBackup(quizcardRepository, writer, path);
             }
 
             QuizCard [] gameQuestions = DataInterface.CreateGame(quizcardRepository);
